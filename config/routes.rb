bcd1c356747558f0f108b5a1f8a7dabe4d38a55e
MaruyamaNao <61174627+MaruyamaNao@users.noreply.github.com>
2020-02-27 15:21:56 +0900

    Revert "Revert "add""
    This reverts commit dd35a5a4355b1256747802dc6c112cd7cf2138d4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+<<<<<<< HEAD
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
+=======
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+>>>>>>> parent of baea23b... Revert "Revert "Revert "Revert "Revert "add"""""
+=======
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+>>>>>>> parent of baea23b... Revert "Revert "Revert "Revert "Revert "add"""""
 end
